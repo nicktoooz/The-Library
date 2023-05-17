e928bccd3fc4ed79959a0514640fe6a775b1c499
Nick_En <95397912+nicktoooz@users.noreply.github.com>
2023-05-17 22:54:25 +0800

    Improved Authentication checker

--- src/firebase/firebase-login.js
+++ src/firebase/firebase-login.js
@@ -1,4 +1,6 @@
 import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js';
+import { getDatabase, ref, onValue } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js';
+
 const auth = getAuth();
 const login = document.getElementById('login-form');
 
@@ -14,7 +16,20 @@ login.addEventListener('submit', (e) => {
       //Minor checking
       //SetValue to remove minor checking if the user logged in again
       //Redirect to Dashboard
-      window.location.href = '/pages/dashboard.html';
+      const database = getDatabase();
+      const role = ref(database, `User Info/Staff/${user.uid}/Role`);
+      onValue(role, (snapshot) => {
+        if (snapshot.exists()) {
+          const status = snapshot.val()
+          console.log(status)
+          if (status === 'Admin') {
+          window.location.replace('/pages/dashboard.html');
+          } else {
+          }
+        } else {
+            console.log(`You don't have enough permission to access the administrator page`);
+        }
+      });
     })
     .catch((error) => {
       const errorCode = error.code;
